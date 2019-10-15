5853c578e8901af2819601b75e789430e92ed3ca
Seven7293 <Seven.dickens@gmail.com>
2019-10-15 22:26:26 +0000

    added input & output values;need to figure out button function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $( "document" ).ready(function() {
-
-
-
+//do pig latin translation
+    $("button").click(function() {
+        let word=$(".input").val();
+        $(".output").append(word);
+    });
 });
